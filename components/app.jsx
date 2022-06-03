08ea2e3a2f2516b5b61bab56fe93d6344e6878d6
Robin Karlsson <robink90@gmail.com>
2022-06-03 16:48:46 +0200

    Release

---
+++ components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  
+  return (
+    <>
+    {/* add your components here */}
+      <h1>Hello World</h1>
+    </>
+  );
+};
+
+export default App;
