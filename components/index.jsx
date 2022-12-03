c160c5abc965f1dc4c8fbd970ec7972feb16d7a1
Robin Karlsson <robink90@gmail.com>
2022-12-03 13:24:46 +0100

    update

--- components/index.jsx
+++ components/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app.jsx';
+import App from './App';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
 root.render(
-      <div>
-        <App />
-      </div>
+  <>
+    <App />
+  </>
 );
